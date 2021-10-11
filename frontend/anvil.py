18b292b87ec8874b75a3faf371ff55f757300caf
Saahil Shangle <saahilshangle@gmail.com>
2021-10-10 20:45:27 -0700

    structure and basics

---
+++ frontend/anvil.py
@@ -0,0 +1,14 @@
+pip install anvil-uplink
+import anvil.server
+
+def connect():
+	anvil.server.connect("AUUM3BBUZ2X663XS3YVCXBOR-A3PANHICIGTZ7P35")
+
+	@anvil.server.callable
+	def say_hello(name):
+  		print("Hello from the uplink, %s!" % name)
+ 		return "Hello from the uplink, %s!" + str(name)
+
+	anvil.server.wait_forever()
+
+connect()
